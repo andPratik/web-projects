7f7224837d0d53459ca755ee4796f76cf5979fcc
pratik thakare <pratikthakre1221@gmail.com>
2023-09-24 00:20:32 +0530

    copy functionality is added

--- 03passwordgenerator/src/App.jsx
+++ 03passwordgenerator/src/App.jsx
@@ -1,11 +1,12 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 
 function App() {
   const [length, setLength] = useState("");
   const [numberAllowed, setnumberAllowed] = useState(false);
   const [charectorAllowed, setcharectorAllowed] = useState(false);
   const [Password, setPassword] = useState("");
-
+  //useref  hook
+  const passwordRef = useRef(null);
   const PasswordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDFGHIHJKLMNOPQUVWXYZabcdefghijklmnopquvwxyz";
@@ -19,6 +20,12 @@ function App() {
     }
     setPassword(pass);
   }, [length, numberAllowed, charectorAllowed, setPassword]);
+
+  const copyPassToClipborad = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(Password)
+  }, [Password]);
+
   useEffect(() => {
     PasswordGenerator();
   }, [length, charectorAllowed, numberAllowed, PasswordGenerator]);
@@ -27,9 +34,20 @@ function App() {
     <div className="bg-gray-800  m-2 m p-8 text-orange-400 flex  flex-col  items-center gap-6 w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3">
       <div className=" text-white text-2xl">Password Generator</div>
       <div className="">
-      <input className=" px-2 py-1 rounded-l-full  overflow-hidden shadow outline-none"
-      type="text" value={Password} placeholder="password" readOnly />
-      <button className="px-2 py-1 rounded-r-full bg-blue-600">copy</button>
+        <input
+          className=" px-2 py-1 rounded-l-full  overflow-hidden shadow outline-none"
+          type="text"
+          value={Password}
+          placeholder="password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          className="px-2 py-1 rounded-r-full bg-blue-600"
+          onClick={copyPassToClipborad}
+        >
+          copy
+        </button>
       </div>
       <div className="flex justify-center items-center gap-2 flex-wrap">
         <input
